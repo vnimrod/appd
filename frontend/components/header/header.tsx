17be30730168174238fdc198f733010f0f4441c9
nimrod vain <togithub1@gmail.com>
2022-06-03 18:11:41 +0300

    create sass variables

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './header.module.scss';
 
 export const Header = () => {
-  return <header className={styles.header}>DO IT</header>;
+  return <header className={styles.header}>DO IT.</header>;
 };
